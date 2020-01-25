79ec5ce68ab903f47d520194b3b95eb96b1d2a7e
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-24 21:21:28 -0600

    Updates

---
+++ precache-manifest.92796097165566c1139f2701a3819d77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3ee8c617995bf109a848d61c000643",
+    "url": "/budget-react-practices/index.html"
+  },
+  {
+    "revision": "8a7e435e308fad2f3754",
+    "url": "/budget-react-practices/static/css/main.61c744dc.chunk.css"
+  },
+  {
+    "revision": "89df3eca611751ca6e3c",
+    "url": "/budget-react-practices/static/js/2.bd931e0c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/budget-react-practices/static/js/2.bd931e0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8a7e435e308fad2f3754",
+    "url": "/budget-react-practices/static/js/main.a24d9d77.chunk.js"
+  },
+  {
+    "revision": "9c04042d36f4f6a5781a",
+    "url": "/budget-react-practices/static/js/runtime-main.f849758a.js"
+  }
+]);
